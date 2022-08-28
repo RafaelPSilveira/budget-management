6b7e9ef837b685faf8121a8f7924de027719aa86
Rafael Silveira <srsilveirapf@gmail.com>
2022-08-28 19:53:29 -0300

    [FIX] ->user registration error fix

--- js/loginRegister.js
+++ js/loginRegister.js
@@ -1,54 +1,48 @@
-btnRegister = document.getElementById('btn-register');
-btnRegister.addEventListener('click', function() {
-
+btnRegister = document.getElementById("btn-register");
+btnRegister.addEventListener("click", function(e) {
+    e.preventDefault();
 
     const register = async() => {
-
         try {
-
-            let user = document.getElementById('user').value;
-            let email = document.getElementById('email').value;
-            let password = document.getElementById('password').value;
-            let confirmPass = document.getElementById('confirm').value
+            let user = document.getElementById("user").value;
+            let email = document.getElementById("email").value;
+            let password = document.getElementById("password").value;
+            let confirmPass = document.getElementById("confirm").value;
 
             if (password != confirmPass) {
-
-                console.log('senhas diferentes!!')
-
+                console.log("senhas diferentes!!");
             } else {
+                var params = { user: user, email: email, password: password };
 
-
-                var params = { 'user': user, 'email': email, 'password': password };
-
-                await axios.post("model/crudUser.php", params, { params: { type: 'create_user' } }, { headers: { 'Content-type': 'application/json' } }).then(() => {
-
-                });
-
+                await axios
+                    .post(
+                        "model/crudUser.php",
+                        params, { params: { type: "create_user" } }, { headers: { "Content-type": "application/json" } }
+                    )
+                    .then((response) => {
+                        const { data } = response;
+                        console.log(data);
+                    });
             }
-
-
         } catch (err) {
             console.log(err);
         }
-    }
+    };
 
     register();
+});
 
-})
-
-const btnLogin = document.querySelector('#faz-login');
-const btnCadastrar = document.querySelector('#faz-cadastro');
-const containerLogin = document.querySelector('#login-container');
-const containerCadastro = document.querySelector('#cadastro-container');
-
-
+const btnLogin = document.querySelector("#faz-login");
+const btnCadastrar = document.querySelector("#faz-cadastro");
+const containerLogin = document.querySelector("#login-container");
+const containerCadastro = document.querySelector("#cadastro-container");
 
 function loginVisivel() {
-    containerLogin.style.display = 'block';
-    containerCadastro.style.display = 'none';
+    containerLogin.style.display = "block";
+    containerCadastro.style.display = "none";
 }
 
 function cadastroVisivel() {
-    containerCadastro.style.display = 'block';
-    containerLogin.style.display = 'none';
+    containerCadastro.style.display = "block";
+    containerLogin.style.display = "none";
 }
--- model/crudUser.php
+++ model/crudUser.php
@@ -2,44 +2,55 @@
     include('connectDB.php');    
     
     $success = [
-        'sugccess' => false,
-        'msg' => 'Error'
+        'success' => false,
+        'data'    => '',
+        'msg'     => 'Error'
     ];   
     
-   
-    $user         = $_REQUEST['user'];
-    $email        = $_REQUEST['email'];
-    $password     = $_REQUEST['password'];
-    $name_include = $_REQUEST['name'];
-    $type         = $_REQUEST['balance'];
-    $category     = $_REQUEST['category'];
-    $value        = $_REQUEST['value'];
+    $_POST = json_decode(file_get_contents("php://input"),true);
     
-
+    
+    $user         = $_POST['user'];
+    $email        = $_POST['email'];
+    $password     = $_POST['password'];
+    // $name_include = $_POST['name'];
+    // $type         = $_POST['balance'];
+    // $category     = $_POST['category'];
+    // $value        = $_POST['value'];
+    $pdo = new PDO('mysql:host='.HOST.';dbname='.DATABASE, USER, PASSWORD);
+    
     try{
-        if(empty($_REQUEST)){
+        if(empty($_POST)){
             $success['success'] = false;
             echo $success['msg'];
         }else{
-            
+            $success['data'] = $user;
+        
             switch ($_REQUEST['type']){
                 
                 case 'create_user':
                     $stmt = $pdo->prepare('INSERT INTO user(user,email,password) VALUES (:user,:email,:password)');
                     $stmt->execute([
-                    'user' => $user,
-                    'email' => $email,
+                    'user'     => $user,
+                    'email'    => $email,
                     'password' => $password,
                     ]);
-                    header('location: ../index.php');
-                    return $success['succes'] = true;
-                
+                    ;
+                    $success['success'] = true;
+                    echo json_encode($success);
+                    // header('location: ../index.php'); 
+                    
+                    return;
+
+                    break; 
+                    
+                default:
+                    return json_encode($success ['msg'] = 'REQUEST Type não foi igual a "creat_user"');
             }
+
         }
     }catch(PDOException $e){
         echo 'Error'.$e->getMessage();                
     }
     
 
-    header("Content-Type: application/json;");
-    echo json_encode($success);
